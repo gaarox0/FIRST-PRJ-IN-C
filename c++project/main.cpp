0e16326caaf7ab8f9c48f698f7f09c5e6936bfd2
Nasser Eddine <nasserel404@gmail.com>
2025-03-03 01:24:52 +0000

    Add files via upload

---
+++ c++project/main.cpp
@@ -0,0 +1,316 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void  A() {
+    int ans;
+
+    printf(" When was BARCELONA founded? \n");
+    printf("1) 1988 \n"
+                 "2) 1877 \n"
+                 "3) 1908 \n"
+                 "4) 1899 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 4 or ans == 1899) {
+        printf("TRUE \n\n");
+    }
+    else {
+        printf("FALSE IS: 1899\n\n");
+    }
+}
+
+void B() {
+    int ans;
+
+    printf("When was REAL MADRID founded? ? \n");
+    printf("1) 1922 \n"
+                 "2) 1902 \n"
+                 "3) 1912 \n"
+                 "4) 1892 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 2 or ans == 1902) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1902\n\n");
+    }
+}
+
+void C() {
+    int ans;
+
+    printf("When was MANCHISTER CITY founded?  \n");
+    printf("1) 1880 \n"
+                 "2) 1890 \n"
+                 "3) 1908 \n"
+                 "4) 1918 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1880) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1880\n\n");
+    }
+}
+
+void D() {
+    int ans;
+
+    printf("When was BAYREN MUNICH founded? \n");
+    printf("1) 1900 \n"
+                 "2) 1910 \n"
+                 "3) 1890 \n"
+                 "4) 1899 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1900) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1900\n\n");
+    }
+}
+
+void E() {
+    int ans;
+
+    printf("When was MANCHESTER UNITED founded ? \n");
+    printf("1) 1897 \n"
+                 "2) 1917 \n"
+                 "3) 1887 \n"
+                 "4) 1878 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 4 or ans == 1878) {
+
+        printf("TRUE IS:1878\n\n");
+
+    }
+    else {
+        printf("FALSE\n\n");
+    }
+}
+void F() {
+    int ans;
+
+    printf("When was DORTMUND founded ? \n");
+    printf("1) 1909 \n"
+                 "2) 1899 \n"
+                 "3) 1919 \n"
+                 "4) 1890 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1909) {
+        printf("TRUE \n\n");
+    }
+    else {
+        printf("FALSE IS/ 1909\n\n");
+    }
+}
+
+void G() {
+    int ans;
+
+    printf("When was PARIS SG founded ? \n");
+    printf("1) 1950 \n"
+                 "2) 1970 \n"
+                 "3) 1940 \n"
+                 "4) 1960 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 2 or ans == 1970) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS:1970\n\n");
+    }
+}
+
+void H() {
+    int ans;
+
+    printf("When was MARSEILLE founded ?  \n");
+    printf("1) 1917 \n"
+                 "2) 1887 \n"
+                 "3) 1897 \n"
+                 "4) 1879 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 3 or ans == 1897) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1897\n\n");
+    }
+}
+
+void I() {
+    int ans;
+
+    printf("When was ARSENAL founded ? \n");
+    printf("1) 1886 \n"
+                 "2) 1889 \n"
+                 "3) 1884 \n"
+                 "4) 1894 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1886) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1886\n\n");
+    }
+}
+
+void J() {
+    int ans;
+
+    printf("When was LIVERPOOL founded ? \n");
+    printf("1) 1894 \n"
+                 "2) 1892 \n"
+                 "3) 1882 \n"
+                 "4) 1912 \n\n");
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 2 or ans == 1892) {
+
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1892\n\n");
+    }
+}
+void  K() {
+    int ans;
+
+    printf("When was ATLETICO MADRID founded ? \n");
+    printf("1) 1923 \n"
+                 "2) 1913 \n"
+                 "3) 1893 \n"
+                 "4) 1903 \n\n");
+
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 4 or ans == 1903) {
+        printf("TRUE \n\n");
+    }
+    else {
+        printf("FALSE IS: 1903\n\n");
+    }
+}
+
+void L() {
+    int ans;
+
+    printf("When was REAL SOCIEDAD founded ? \n");
+    printf("1) 1909 \n"
+                 "2) 1899 \n"
+                 "3) 1907 \n"
+                 "4) 1919 \n\n");
+
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1909) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1909\n\n");
+    }
+}
+
+void M() {
+    int ans;
+
+    printf("When was AJAX AMSTERDAM founded ? \n");
+    printf("1) 1899 \n"
+                 "2) 1900 \n"
+                 "3) 1910 \n"
+                 "4) 1890 \n");
+
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 2 or ans == 1900) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1900\n\n");
+    }
+}
+
+void N() {
+    int ans;
+
+    printf("When was ATHLETIC BILBAO founded? \n");
+    printf("1) 1900 \n"
+                 "2) 1910 \n"
+                 "3) 1890 \n"
+                 "4) 1880 \n\n");
+
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1890) {
+        printf("TRUE\n\n");
+    }
+    else {
+        printf("FALSE IS: 1890\n\n");
+    }
+}
+
+void O() {
+    int ans;
+
+    printf("When was BAYERN LEVERKUSEN founded ? \n");
+    printf("1) 1904 \n"
+                 "2) 1907 \n"
+                 "3) 1894 \n"
+                 "4) 1897 \n\n");
+
+    printf("==>");
+    scanf("%d", &ans);
+
+    if (ans == 1 or ans == 1904 ) {
+
+        printf("TRUE IS: 1904\n\n");
+
+    }
+    else {
+        printf("FALSE\n\n");
+    }
+}
+
+int main() {
+    printf("----|   When was your team founded?   |----\n\n\n");
+
+    A();
+    B();
+    C();
+    D();
+    E();
+    F();
+    J();
+    H();
+    I();
+    K();
+    L();
+    M();
+    N();
+    O();
+
+return 0;
+}
